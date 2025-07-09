0b8f3e6d010956d5cafe12d30e3e0340e4c50083
stainbrainio <kaunghtetkoko29102006@gmail.com>
2025-07-09 12:10:35 -0400

    Day3
    day3 work

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour{
+
+    int varTwo = 3;
+    void Start()
+    {  
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    Debug.Log(varTwo);
+    varTwo++;
+    }
+}
