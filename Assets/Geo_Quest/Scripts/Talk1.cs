bbbc829ccb065f9a2e9fe0f06e850fe565a8e372
stainbrainio <kaunghtetkoko29102006@gmail.com>
2025-07-14 12:14:45 -0400

    day6

--- Assets/Geo_Quest/Scripts/Talk1.cs
+++ Assets/Geo_Quest/Scripts/Talk1.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Talk1 : MonoBehaviour
 {
@@ -9,7 +10,28 @@ public class Talk1 : MonoBehaviour
     {
         
     }
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit"); Debug.Log(collision.tag);
+        switch (collision.tag)
+        {
+            case "Win":
+                {
+                    break;
+                }
+            case "Death": 
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                
+                
+                }
+        
+        }
+    }
+        
+   
     // Update is called once per frame
     void Update()
     {
--- Assets/Geo_Quest/Scripts/moving1.cs
+++ Assets/Geo_Quest/Scripts/moving1.cs
@@ -14,15 +14,16 @@ public class moving1 : MonoBehaviour
     }
 
     // Update is called once per frame
+   
     void Update()
     {
-        rb.velocity = Vector2.left;
+       
         
         float xInput = Input.GetAxis("Horizontal");
 
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
 
-        Debug.Log(xInput);  
+        
 
 
 
