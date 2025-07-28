d0977ead92042fcc0a2970f6f4c6be6469612a50
stainbrainio <kaunghtetkoko29102006@gmail.com>
2025-07-28 12:17:45 -0400

    regegre

---
+++ Assets/Pixel_Quest/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public string nextLevel;
+
+    public void LoadLevel()
+    {
+
+        SceneManager.LoadScene(nextLevel);
+
+
+
+
+
+
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+
+
+    }
+
+
+
+
+}
